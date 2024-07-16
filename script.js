f3748f4c631e7b19cc9942b50dbd4c10b0cfa321
Isaac Cobby Andoh <cobbjy@gmail.com>
2024-07-16 04:47:27 -0400

    new updates

--- script.js
+++ script.js
@@ -46,11 +46,11 @@ document.addEventListener('DOMContentLoaded', function() {
         storedTasks.forEach(taskText => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
     }
 
-    addButton.addEventListener('click', () =>
-      addTask(taskInput.value));
+addButton.addEventListener('click', addTask);
     taskInput.addEventListener('keypress', (event) => {
         if (event.key === 'Enter') {
-            addTask(taskInput.value);
+            addTask();
+
         }
     });
 
